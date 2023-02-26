8b0fa6f221f0336733567fe306280da4fefbe058
Jiexin Kuang <jxkuang@student.ubc.ca>
2023-02-26 15:34:40 -0800

    save_20230226

--- milestone_2/grab_books.py
+++ milestone_2/grab_books.py
@@ -4,13 +4,10 @@ from os import listdir
 from os.path import isfile, join
 from create_template import *
 
-html_path = "../data/examples"  # change the path to the folder which contains html files (eg. "../data/concat")
+html_path = "../data/concat"  # change the path to the folder which contains html files (eg. "../data/concat")
 all_books = "../data/possi_books/"  # output folder
 index_to_link = '../data/index_to_link.json'
 
-
-onlyfiles = [f for f in listdir(html_path) if f.endswith('.html')]
-print(onlyfiles)
 
 
 def grab_possible_book():
@@ -19,16 +16,17 @@ def grab_possible_book():
 
     for file in listdir(html_path):
         if isfile(join(html_path, file)) and file.endswith('.html'):
-            with open(join(html_path, file)) as fp:
-                soup = BeautifulSoup(fp, 'html.parser')
-                i = file.split('.')[0]
+            i = file.split('.')[0]
+            if 1500 <= int(i) <= 1750:
+                with open(join(html_path, file)) as fp:
+                    soup = BeautifulSoup(fp, 'html.parser')
 
-                # Get all italics
-                all_italics = soup.findChildren('em')
+                    # Get all italics
+                    all_italics = soup.findChildren('em')
 
-                with open(all_books + i + '.txt', 'w') as f:
-                    for item in all_italics:
-                        f.write(item.parent.text+"\n\n")
+                    with open(all_books + i + '.txt', 'w') as f:
+                        for item in all_italics:
+                            f.write(item.parent.text+"\n\n")
 
 
 if __name__ == "__main__":
