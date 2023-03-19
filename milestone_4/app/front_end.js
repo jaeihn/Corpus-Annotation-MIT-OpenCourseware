f8dea215fc5b864e180c360f3b396da5d9b799d2
Min Zeng <ceciliaminzeng@gmail.com>
2023-03-19 12:50:09 -0700

    adjust_hero_blocks_buttons_20230319_v1

---
+++ milestone_4/app/front_end.js
@@ -0,0 +1,44 @@
+
+
+function checkHappy()
+{
+     var checkbox = document.getElementById("happycheckbox");
+     alert(checkbox.checked);
+     if (checkbox.checked == false) {
+        alert("You didn't say you were happy!");
+    } 
+    else {
+	var form = document.getElementById("form");
+        form.submit();
+    }
+}
+
+function update_page(response) {
+  	document.getElementById('rightbox').innerHTML = response
+}
+
+
+function addDiv()
+{
+	var form = document.getElementById("form");
+	const formData = new FormData(form);
+	const searchParams = new URLSearchParams(formData);
+	const queryString = searchParams.toString();
+        alert(queryString);
+	xmlHttpRqst = new XMLHttpRequest( )
+	xmlHttpRqst.onload = function(e) {update_page(xmlHttpRqst.response);} 
+	xmlHttpRqst.open( "GET", "/pos?" + queryString);
+	try {
+		xmlHttpRqst.send( null );
+	}
+	catch(err)
+	{
+		alert(err);
+	}
+}
+
+function turnGreen() {
+   var thing = document.getElementById("toturngreen");
+   thing.setAttribute("class", "cell");
+   thing.innerHTML = "I am!";
+}
